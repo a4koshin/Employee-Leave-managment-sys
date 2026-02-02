3c258f96b834dd40ccbe7c8a6f161777cd23f7c1
a4koshin <mankajr11@gmail.com>
2026-02-02 21:17:49 +0300

    DB directory is added

---
+++ db/init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS users (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  fullname VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  role ENUM('employee', 'admin') NOT NULL DEFAULT 'employee',
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  UNIQUE KEY uniq_users_email (email)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
