87ac91c52bca84134e0bbc98e7547c64e878241e
a4koshin <mankajr11@gmail.com>
2026-02-02 18:23:11 +0300

    Register page is created

--- src/public/register.php
+++ src/public/register.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . "/../config/db.php";
-require_once __DIR__ . "/../src/helpers/auth.php";
+require_once __DIR__ . "/../helpers/auth.php";
 
 $error = "";
 $success = "";
